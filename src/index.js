08675cb1be6211abd227906c59f65db65361360a
Tayyab Haseeb <tayyabhaseeb5@gmail.com>
2023-10-30 18:46:35 +0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+// import StarList from "./StarList";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// function RatingCont() {
+//   const [localRating, setLocalRating] = useState(0);
+//   return (
+//     <div>
+//       <StarList
+//         maxRating={5}
+//         color="red"
+//         size="20"
+//         setUserRating={setLocalRating}
+//       />
+//       <p>The rating is {localRating}</p>
+//     </div>
+//   );
+// }
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    {/* <App /> */}
+    {/* <StarList maxRating={5} />
+    <StarList maxRating={10} color="red" size="20" />
+    <StarList
+      maxRating={5}
+      color="blue"
+      size="80"
+      message={["poor", "bad", "average", "Good", "Excellent"]}
+    /> */}
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
